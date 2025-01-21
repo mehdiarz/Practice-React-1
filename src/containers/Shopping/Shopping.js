169e0b65dbc5f52390f2de8183ace990f63dd6a9
yacilii <yasiarz30@gmail.com>
2025-01-21 09:12:34 +0330

    second

---
+++ src/containers/Shopping/Shopping.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Wrapper from "../../hoc/Wrapper";
+
+class Shopping extends React.Component{
+    render(){
+        return(
+            <Wrapper>
+            <div>controls</div>
+        </Wrapper>
+        )
+
+    }
+}
+
+export default Shopping
